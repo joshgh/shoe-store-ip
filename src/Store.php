973a4eb3ae4296310d2c2c68dfa44d92878ea937
Joshua Huffman <j.m.huffman@gmail.com>
2016-09-30 09:32:11 -0700

    write constructor and getters for Store, getStoreName passes test

--- src/Store.php
+++ src/Store.php
@@ -3,6 +3,22 @@
     {
         private $id;
         private $store_name;
+
+        function __construct($store_name, $id = null)
+        {
+            $this->store_name = $store_name;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getStoreName()
+        {
+            return $this->store_name;
+        }
     }
 
 ?>
