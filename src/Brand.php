4420b8d15396184674bcb7e578f7e6bf97cb1a10
Joshua Huffman <j.m.huffman@gmail.com>
2016-09-30 10:34:05 -0700

    add constructor, getters, save, getAll, deleteAll, find methods for Brand, tests pass

--- src/Brand.php
+++ src/Brand.php
@@ -1,6 +1,58 @@
 <?php
     class Brand
     {
-        
+        private $id;
+        private $brand_name;
+
+        function __construct($brand_name, $id = null)
+        {
+            $this->brand_name = $brand_name;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getBrandName()
+        {
+            return $this->brand_name;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (brand_name) VALUES ('{$this->brand_name}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands");
+            $brand_array = array();
+            foreach ($returned_brands as $brand){
+                $id = $brand['id'];
+                $brand_name = $brand['brand_name'];
+                $new_brand = new Brand($brand_name, $id);
+                array_push($brand_array, $new_brand);
+            }
+            return $brand_array;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brands;");
+        }
+
+        static function find($search_id)
+        {
+            $returned_brand = $GLOBALS['DB']->query("SELECT * FROM brands WHERE id = {$search_id}");
+            $brand = null;
+            $brand_array = $returned_brand->fetch(PDO::FETCH_ASSOC);
+            if ($brand_array){
+                $brand = new Brand($brand_array['brand_name'], $brand_array['id']);
+            }
+            return $brand;
+        }
     }
 ?>
